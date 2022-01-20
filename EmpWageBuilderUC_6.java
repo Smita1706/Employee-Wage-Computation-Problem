44cae0e0aa96b354bc2f6ff9d94e5e885de232b7
Smita1706 <smitachinchkar1998@gmail.com>
2022-01-20 22:09:39 +0530

    Updated : Employee wage computation program use case 6 updated successfully.

--- EmpWageBuilderUC_6.java
+++ EmpWageBuilderUC_6.java
@@ -1,46 +1,40 @@
 package WageComputation;
 
 public class EmpWageBuilderUC_6 {
-	 public static void main(String[] args) {
-	      // Constants
-		  int IS_PART_TIME=1;
-	      int IS_FULL_TIME = 2;
-		  int WAGE_PER_HOUR =20;
-		  int NO_OF_DAYS=20;
-		  int MAX_HOURS_IN_MONTH=100;
-		  
-		  //variables
-		  int WORKING_HOUR=0;
-		  int TOTAL_WORKING_HOUR=0;
-		  int TOTAL_SALARY=0;
-		  int SALARY=0;
-		  int i=0;
-		  
-	      
-	      System.out.println("Welcome to Employee Wage Computaion");
-	     // Computation
-	        while(i < NO_OF_DAYS && TOTAL_WORKING_HOUR <= MAX_HOURS_IN_MONTH) {
-	    	   double empCheck = Math.floor(Math.random() * 10) % 3;
-	    	   int Check= (int)empCheck;
-	    	   switch(Check) {
-	    	   
-	    	   case 1:
-	        	       WORKING_HOUR=8;
-	        	       break;
-	    	   case 2 :
-	                   WORKING_HOUR=4;
-	                   break;
-	           default : 
-	        	        WORKING_HOUR=0;
-	        	        break;
-	       }
-	    	   i++;
-	    	  SALARY= WAGE_PER_HOUR*WORKING_HOUR;
-	    	  TOTAL_WORKING_HOUR+=WORKING_HOUR;
-	    	   TOTAL_SALARY+=SALARY;
-	    	   System.out.println("salary is  : " + SALARY);
-	    }
-	        System.out.println("total working hours are  : " + TOTAL_WORKING_HOUR);
-	         System.out.println("total salary is  : " + TOTAL_SALARY);
-  }
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int WAGE_PER_HOUR = 20;
+	public static final int NO_OF_DAYS = 20;
+	public static final int MAX_HOURS_IN_MONTH = 100;
+
+	public static void main(String[] args) {
+		int WorkingHour = 0;
+		int TotalWorkingHour = 0;
+		int TotalSalary = 0;
+		int Salary = 0;
+		int Days = 0;
+		int EmpCheck;
+
+		while(Days < NO_OF_DAYS && TotalWorkingHour <= MAX_HOURS_IN_MONTH) {
+			EmpCheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch(EmpCheck) {
+			case 1:
+				WorkingHour = 8;
+				break;
+			case 2:
+				WorkingHour = 4;
+				break;
+			default: 
+				WorkingHour = 0;
+				break;
+			}
+			Days++;
+			Salary = WAGE_PER_HOUR * WorkingHour;
+			TotalWorkingHour = TotalWorkingHour + WorkingHour;
+			TotalSalary = TotalSalary + Salary;
+			System.out.println("salary is  : " + Salary);
+		}
+		System.out.println("total working hours are  : " + TotalWorkingHour);
+		System.out.println("total salary is  : " + TotalSalary);
+	}
 }
