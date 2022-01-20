8ac7dd2f46e9d750cdebbebd34142d38fbfba046
Smita1706 <smitachinchkar1998@gmail.com>
2022-01-20 22:09:07 +0530

    Updated : Employee wage computation program use case 5 updated successfully.

--- EmpWageBuilderUC_5.java
+++ EmpWageBuilderUC_5.java
@@ -1,40 +1,36 @@
 package WageComputation;
 
 public class EmpWageBuilderUC_5 {
-	 // Constants
-	public static final int IS_PART_TIME=1;
+	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
-	public static final int WAGE_PER_HOUR =20;
-	public static final int NO_OF_DAYS=20;
-	
-	 public static void main(String[] args) {		  
-		  //variables
-		  int WORKING_HOUR=0;
-		  int TOTAL_SALARY=0;
-		  int SALARY=0;
-		  
-	      
-	      System.out.println("Welcome to Employee Wage Computaion");
-	     // Computation
-	         for(int days=0 ;days<NO_OF_DAYS ;days++) {
-	    	   double empCheck = Math.floor(Math.random() * 10) % 3;
-	    	   int Check= (int)empCheck;
-	    	   switch(Check) {
-	    	   
-	    	   case 1:
-	        	       WORKING_HOUR=8;
-	        	       break;
-	    	   case 2 :
-	                   WORKING_HOUR=4;
-	                   break;
-	           default : 
-	        	        WORKING_HOUR=0;
-	        	        break;
-	       }
-	    	  SALARY= WAGE_PER_HOUR*WORKING_HOUR;
-	    	   TOTAL_SALARY+=SALARY;
-	    	   System.out.println("salary is  : " + SALARY);
-	    }
-	         System.out.println("total salary is  : " + TOTAL_SALARY);
-  }
+	public static final int WAGE_PER_HOUR = 20;
+	public static final int NO_OF_DAYS = 20;
+
+	public static void main(String[] args) {		  
+		int WorkingHour = 0;
+		int TotalSalary = 0;
+		int Salary = 0;
+		int EmpCheck;
+		int Days;
+
+		for(Days=0 ;Days < NO_OF_DAYS ;Days++) {
+			EmpCheck = (int) Math.floor(Math.random() * 10) % 3;
+
+			switch(EmpCheck) {
+			case 1:
+				WorkingHour = 8;
+				break;
+			case 2:
+				WorkingHour = 4;
+				break;
+			default: 
+				WorkingHour = 0;
+				break;
+			}
+			Salary = WAGE_PER_HOUR * WorkingHour;
+			TotalSalary = TotalSalary + Salary;
+			System.out.println("salary is  : " + Salary);
+		}
+		System.out.println("total salary is  : " + TotalSalary);
+	}
 }
