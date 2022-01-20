b2d66b5accea11994fb1b22b1672af069715baf6
Smita1706 <smitachinchkar1998@gmail.com>
2022-01-20 22:08:41 +0530

    Updated : Employee wage computation program use case 4 updated successfully.

--- EmpWageBuilderUC_4.java
+++ EmpWageBuilderUC_4.java
@@ -1,31 +1,28 @@
 package WageComputation;
 
 public class EmpWageBuilderUC_4 {
-	 // Constants
-	  public static final int IS_PART_TIME=1;
-	  public static final int IS_FULL_TIME = 2;
-	  public static final int WAGE_PER_HOUR =20;
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int WAGE_PER_HOUR = 20;
+
 	public static void main(String[] args) {
-		
-		 //variables
-		  int WORKING_HOUR=0;
-		  int TOTAL_SALARY=0;
-	      
-	     // Computation 
-	    	   int empCheck =(int) Math.floor(Math.random() * 10) % 3;
-	    	   switch(empCheck) {
-	    	   
-	    	   case 1:
-	        	       WORKING_HOUR=8;
-	        	       break;
-	    	   case 2 :
-	                   WORKING_HOUR=4;
-	                   break;
-	           default : 
-	        	        WORKING_HOUR=0;
-	        	        break;
-	       }
-	    	   TOTAL_SALARY= WAGE_PER_HOUR*WORKING_HOUR;
-	    	   System.out.println("salary is  : " + TOTAL_SALARY);
-  }
+		int WorkingHour = 0;
+		int TotalSalary = 0;
+		int EmpCheck;
+
+		EmpCheck = (int) Math.floor(Math.random() * 10) % 3;
+		switch(EmpCheck) {
+		case 1:
+			WorkingHour = 8;
+			break;
+		case 2:
+			WorkingHour = 4;
+			break;
+		default: 
+			WorkingHour = 0;
+			break;
+		}
+		TotalSalary = WAGE_PER_HOUR * WorkingHour;
+		System.out.println("salary is  : " + TotalSalary);
+	}
 }
