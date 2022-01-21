c67ffe1558e341608b79bbf7d147111b4fabea4f
Smita1706 <smitachinchkar1998@gmail.com>
2022-01-21 17:27:49 +0530

    Updated : Use case 1 in branch EmployeeAttendanceUC1 is updated successfully

--- EmpWageBuilderUC_1.java
+++ EmpWageBuilderUC_1.java
@@ -6,7 +6,7 @@ public class EmpWageBuilderUC_1 {
 		// Constants
 		int IS_FULL_TIME = 1;
 
-		// Computation
+	
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if (empCheck == IS_FULL_TIME)
 		   System.out.println("Employee is Present");
